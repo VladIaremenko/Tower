f9c3d95ddd7c6ebb76ebb31a9a58beaf98dbc5a3
Vlad <vlad.iaremenko@gmail.com>
2023-03-16 14:31:50 +0300

    Items updates

---
+++ Assets/_Scripts/_General/AppInitiator.cs
@@ -0,0 +1,17 @@
+﻿using Tower.Assets._Scripts._Wave;
+using UnityEngine;
+
+namespace Tower.Assets._Scripts._General
+{
+    public class AppInitiator : MonoBehaviour
+    {
+        [SerializeField] private WaveManagerSO _waveManager;
+
+        private void Start()
+        {
+            _waveManager.Init(this);   
+        }
+    }
+}
+
+
---
+++ Assets/_Scripts/_Wave/WaveManagerSO.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Tower.Assets._Scripts._Wave
+{
+    [CreateAssetMenu(fileName = "WaveManagerSO", menuName = "SO/Wave/WaveManagerSO", order = 1)]
+    public class WaveManagerSO : ScriptableObject
+    {
+        [SerializeField] private float WaveDelayTime;
+        private MonoBehaviour _mono;
+
+        public void Init(MonoBehaviour mono)
+        {
+            _mono = mono;
+            _mono.StartCoroutine(WaveCoroutine());
+        }
+
+        private IEnumerator WaveCoroutine()
+        {
+            float time = 0f;
+
+            while (true)
+            {
+                time += Time.deltaTime;
+
+                if(time > WaveDelayTime)
+                {
+                    Debug.Log("StartNextWave");
+                    time = 0f;
+                }
+
+                yield return new WaitForEndOfFrame();
+            }
+        }
+    }
+}
+
+
